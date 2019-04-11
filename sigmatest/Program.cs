c3a48df31c1f8c4fa8a4b2f2e7b9b51ff0767941
Roma <romagapunich@gmail.com>
2019-04-11 20:24:53 +0300

    fst commit

---
+++ sigmatest/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections;
+using System.Runtime.CompilerServices;
+
+namespace sigmatest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            Console.WriteLine("Hello World!");
+        }
+    }
+
+    public class MyList : IEnumerable
+    {
+        private readonly int[] items;
+        MyList(params int[] _items)
+        {
+            items = _items;
+        }
+
+        public IEnumerable GetEnumerable() => items.GetEnumerator();
+    }
+}
